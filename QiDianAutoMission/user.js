b1e05d26288dd84d227cc0794046623b7ca36534
Sunrishe <gsunrishe@gmail.com>
2018-08-01 17:51:25 +0800

    修改起点自动任务脚本的文件路径

--- QiDianAutoMission/user.js
+++ QiDianAutoMission/user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         起点任务
-// @namespace    https://github.com/sunrishe/Tampermonkey/
+// @namespace    https://github.com/sunrishe/Tampermonkey/QiDianAutoMission
 // @namespace    https://greasyfork.org/zh-CN/users/200077-sunrishe
 // @version      1.0
 // @description  起点中文网自动挂机、领取在线经验、完成每日任务的油猴脚本
